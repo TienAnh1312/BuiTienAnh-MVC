ab571dc7f9f1139a26adab551184801d075b35b5
TienAnh1312 <buitienanh13122003@gmail.com>
2024-11-28 20:36:05 +0700

    L10_Authorrize_2

---
+++ L10_CustomAuthorize/L10_CustomAuthorize/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L10_CustomAuthorize.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
